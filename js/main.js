e5324d32b018c6f1e0a8c78bc74e77047e69e7ce
omairapalacios <cpalaciosanchez@hotmail.com>
2020-02-17 10:16:15 -0500

    update styles

--- js/main.js
+++ js/main.js
@@ -13,9 +13,6 @@
     },
 
     $WIN = $(window);
-
-    // Add the User Agent to the <html>
-    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
@@ -87,18 +84,7 @@
             }
 
         });
-
-        // $WIN.on('resize', function() {
-        //     if ($WIN.width() <= 768) {
-        //             hdr.removeClass('sticky offset scrolling');
-        //     }
-        // });
-
     };
-
-
-    /* Mobile Menu
-     * ---------------------------------------------------- */ 
     var ssMobileMenu = function() {
 
         var toggleButton = $('.header-menu-toggle'),
